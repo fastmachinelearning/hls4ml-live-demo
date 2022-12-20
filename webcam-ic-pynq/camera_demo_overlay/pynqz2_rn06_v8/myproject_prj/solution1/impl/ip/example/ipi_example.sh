e3bdf02341d54bc0539fdf04d75d32955eceda53
mrigatti <103663080+mrigatti@users.noreply.github.com>
2022-12-20 15:24:22 -0600

    Add files via upload

---
+++ webcam-ic-pynq/camera_demo_overlay/pynqz2_rn06_v8/myproject_prj/solution1/impl/ip/example/ipi_example.sh
@@ -0,0 +1,6 @@
+# ==============================================================
+# Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2019.2.1 (64-bit)
+# Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+# ==============================================================
+
+/data/Xilinx/Vivado/2019.2/bin/vivado  -notrace -mode batch -source ipi_example.tcl -tclargs xc7z020-clg400-1 ../xilinx_com_hls_myproject_axi_1_0.zip
