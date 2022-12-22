23c83628f1685fca7fb00fc7df688f63247e6a89
mrigatti <103663080+mrigatti@users.noreply.github.com>
2022-12-22 05:55:08 -0600

    Add files via upload

---
+++ webcam-ic-pynq/camera_demo_overlay/pynqz2_rn06_v8/myproject_prj/solution1/syn/verilog/shift_line_buffer_array_ap_fixed_32u_config7_s_line_buffehbi.v
@@ -0,0 +1,81 @@
+// ==============================================================
+// Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2019.2.1 (64-bit)
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// ==============================================================
+
+`timescale 1 ns / 1 ps
+
+module shift_line_buffer_array_ap_fixed_32u_config7_s_line_buffehbi_core (
+    clk,
+    ce,
+    din,
+    addr,
+    dout);
+
+parameter DATA_WIDTH = 32'd8;
+parameter ADDR_WIDTH = 32'd4;
+parameter DEPTH = 32'd10;
+
+input clk;
+input ce;
+input [DATA_WIDTH-1:0] din;
+input [ADDR_WIDTH-1:0] addr;
+output [DATA_WIDTH-1:0] dout;
+
+reg[DATA_WIDTH-1:0] ShiftRegMem[0:DEPTH-1];
+integer i;
+
+initial
+begin
+    for(i=0;i<DEPTH;i=i+1)
+        ShiftRegMem[i] <= {DATA_WIDTH{1'b0}};
+end
+
+always @ (posedge clk)
+begin
+    if (ce)
+    begin
+        for(i=0;i<DEPTH-1;i=i+1)
+            ShiftRegMem[i+1] <= ShiftRegMem[i];
+        ShiftRegMem[0] <= din;
+    end
+end
+
+assign dout = ShiftRegMem[addr];
+
+endmodule
+
+module shift_line_buffer_array_ap_fixed_32u_config7_s_line_buffehbi (
+    clk,
+    reset,
+    address0,
+    ce0,
+    we0,
+    d0,
+    q0);
+
+parameter DataWidth = 32'd8;
+parameter AddressRange = 32'd10;
+parameter AddressWidth = 32'd4;
+
+input clk;
+input reset;
+input [AddressWidth-1:0] address0;
+input ce0;
+input we0;
+input [DataWidth-1:0] d0;
+output [DataWidth-1:0] q0;
+
+shift_line_buffer_array_ap_fixed_32u_config7_s_line_buffehbi_core #(
+    .DATA_WIDTH( DataWidth ),
+    .ADDR_WIDTH( AddressWidth ),
+    .DEPTH( AddressRange ))
+shift_line_buffer_array_ap_fixed_32u_config7_s_line_buffehbi_core_U(
+    .clk(clk),
+    .ce(we0),
+    .din(d0),
+    .addr(address0),
+    .dout(q0)
+);
+
+endmodule
