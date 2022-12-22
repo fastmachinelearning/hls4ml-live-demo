2d6281a20924b2d3ea28087db89d8b2c7eaa795c
mrigatti <103663080+mrigatti@users.noreply.github.com>
2022-12-22 06:17:50 -0600

    Add files via upload

---
+++ webcam-ic-pynq/camera_demo_overlay/pynqz2_rn06_v8/myproject_vivado_accelerator/project_1.srcs/sources_1/bd/design_1/ipshared/0550/hdl/verilog/conv_2d_cl_array_array_ap_fixed_32u_config7_s_w5_V.v
@@ -0,0 +1,64 @@
+// ==============================================================
+// Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2019.2.1 (64-bit)
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// ==============================================================
+`timescale 1 ns / 1 ps
+module conv_2d_cl_array_array_ap_fixed_32u_config7_s_w5_V_rom (
+addr0, ce0, q0, clk);
+
+parameter DWIDTH = 5;
+parameter AWIDTH = 14;
+parameter MEM_SIZE = 9216;
+
+input[AWIDTH-1:0] addr0;
+input ce0;
+output reg[DWIDTH-1:0] q0;
+input clk;
+
+reg [DWIDTH-1:0] ram[0:MEM_SIZE-1];
+
+initial begin
+    $readmemh("./conv_2d_cl_array_array_ap_fixed_32u_config7_s_w5_V_rom.dat", ram);
+end
+
+
+
+always @(posedge clk)  
+begin 
+    if (ce0) 
+    begin
+        q0 <= ram[addr0];
+    end
+end
+
+
+
+endmodule
+
+`timescale 1 ns / 1 ps
+module conv_2d_cl_array_array_ap_fixed_32u_config7_s_w5_V(
+    reset,
+    clk,
+    address0,
+    ce0,
+    q0);
+
+parameter DataWidth = 32'd5;
+parameter AddressRange = 32'd9216;
+parameter AddressWidth = 32'd14;
+input reset;
+input clk;
+input[AddressWidth - 1:0] address0;
+input ce0;
+output[DataWidth - 1:0] q0;
+
+
+
+conv_2d_cl_array_array_ap_fixed_32u_config7_s_w5_V_rom conv_2d_cl_array_array_ap_fixed_32u_config7_s_w5_V_rom_U(
+    .clk( clk ),
+    .addr0( address0 ),
+    .ce0( ce0 ),
+    .q0( q0 ));
+
+endmodule
+
