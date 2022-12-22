b58305fadd73fd99d6c2ac8d045e23188d774016
mrigatti <103663080+mrigatti@users.noreply.github.com>
2022-12-22 06:01:18 -0600

    Add files via upload

---
+++ webcam-ic-pynq/camera_demo_overlay/pynqz2_rn06_v8/myproject_vivado_accelerator/project_1.cache/ip/2019.2.1/c91fe024d9db182b/design_1_myproject_axi_0_0_stub.vhdl
@@ -0,0 +1,39 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2.1 (lin64) Build 2729669 Thu Dec  5 04:48:12 MST 2019
+-- Date        : Tue Aug  9 17:27:50 2022
+-- Host        : correlator2.fnal.gov running 64-bit Scientific Linux release 7.9 (Nitrogen)
+-- Command     : write_vhdl -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+--               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ design_1_myproject_axi_0_0_stub.vhdl
+-- Design      : design_1_myproject_axi_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+  Port ( 
+    in_r_TVALID : in STD_LOGIC;
+    in_r_TREADY : out STD_LOGIC;
+    in_r_TDATA : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    in_r_TLAST : in STD_LOGIC_VECTOR ( 0 to 0 );
+    out_r_TVALID : out STD_LOGIC;
+    out_r_TREADY : in STD_LOGIC;
+    out_r_TDATA : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    out_r_TLAST : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ap_clk : in STD_LOGIC;
+    ap_rst_n : in STD_LOGIC
+  );
+
+end decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix;
+
+architecture stub of decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "in_r_TVALID,in_r_TREADY,in_r_TDATA[31:0],in_r_TLAST[0:0],out_r_TVALID,out_r_TREADY,out_r_TDATA[31:0],out_r_TLAST[0:0],ap_clk,ap_rst_n";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "myproject_axi,Vivado 2019.2.1";
+begin
+end;
